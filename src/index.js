b5fdb0f441941c19e1203a45a5b34c4a430641f4
JuanfeMon23 <ELFuanfex@hotmail.com>
2023-08-20 15:37:29 -0500

    importing models

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import app from './app.js'
 import { sequelize } from './database/database.js';
-import './models/Users.model.js'
-import './models/Roles.model.js'
-
+import './models/Roles.model.js';
+import './models/Users.model.js';
+import './models/Licenses.model.js'
+import './models/LicensesRoles.model.js'
 
 const connection = async (req,res) => {
     try {
-        await sequelize.sync();
+        await sequelize.sync({force:  true});
         app.listen(3000);
         console.log('Server is running on port 3000');
+
     } catch (error) {
-        res.send(error.message)
+
     }
 }
 
