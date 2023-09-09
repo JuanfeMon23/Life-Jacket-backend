3e6950e1468a5fa1e00a620edf7230c29195eb7c
Yenifer Salazar <yensalazarrestrepo@gmail.com>
2023-09-09 12:26:13 -0500

    changes in controller, model of Sales

--- src/controllers/Sales.controller.js
+++ src/controllers/Sales.controller.js
@@ -1,12 +1,23 @@
 import {Sale} from '../models/Sales.model.js';
-// import {Vehicle} from '../models/Vehicles.model.js';
 import {Client} from '../models/Clients.model.js';
+import {Vehicle} from '../models/Vehicles.model.js';
 import { Op } from 'sequelize';
 import app from '../app.js';
 
 export const getSales = async (req, res) => {
     try {
-        const sales = await Sale.findAll();
+        const sales = await Sale.findAll({
+            include: [
+                {
+                    model : Client
+                },
+                {
+                    model: Vehicle, 
+                    as: 'vehiclesAs' // nombre de la asosiacion con Sale
+                },
+                
+            ],
+        });
         res.json(sales);
     } catch (error) {
         console.error(error);
@@ -21,9 +32,24 @@ export const getSale = async (req,res) => {
         const sale = await Sale.findOne({
             where: {
                 idSale
-            }
-        })
-        res.json(sale)
+            },
+            include: [
+                {
+                    model : Client
+                },
+                {
+                    model: Vehicle, 
+                    as: 'vehiclesAs'
+                },
+            ],
+        });
+
+        if (sale) {
+            const vehiclesAss = sale.SaleDetails;
+            res.json({ sale, vehiclesAss });
+        } else {
+            return res.status(404).json({ message: 'Venta no encontrada' });
+        }
     } catch (error) {
         return res.status(500).json({message : error.message});
 
@@ -32,7 +58,7 @@ export const getSale = async (req,res) => {
 
 export const postSale = async (req, res) => {
     try {
-        const {saleDate, salePrice, salePaymentMethod, saleLimitations, saleCity, salePecuniaryPenalty, saleStatus, idClientSale} = req.body;
+        const {saleDate, salePrice, salePaymentMethod, saleLimitations, saleCity, salePecuniaryPenalty, saleStatus, idClientSale, vehicleId} = req.body;
 
         const newSale = await Sale.create({
             saleDate,
@@ -42,16 +68,22 @@ export const postSale = async (req, res) => {
             saleCity,
             salePecuniaryPenalty,
             saleStatus,
-            idClientSale
+            idClientSale,
+            vehicle : [vehicleId]
         });
-/* 
-        Sale.afterCreate(async (sale) => {
-            const vehicles = await sale.getVehicles();
-            await Promise.all(vehicles.map((Vehicle) => Vehicle.update({ vehicleStatus : false })));
-          }); */
 
+    try {
+    const vehicle = await Vehicle.findByPk(vehicleId);
+    if (vehicle) {
+        await newSale.setVehicles([vehicle]);
+    } else {
+        console.error('No se pudo encontrar el vehículo.');
+    }
+    } catch (error) {
+    console.error('Error al buscar el vehículo:', error);
+    }
 
-        return res.status(200).json(newSale);
+    return res.status(200).json(newSale);
     } catch (error) {
         return res.status(500).json({message : error.message});
     }
@@ -98,13 +130,32 @@ export const searchSale = async (req, res) => {
     const {search} = req.params;
     try {
         const sale = await Sale.findAll({
-            where : {
+            include: [
+                {
+                    model: Client, 
+                },
+                {
+                    model: Vehicle, 
+                    as: 'vehiclesAs',
+                }
+            ],
+            where: {
                 [Op.or]: [
-                    {saleDate : { [Op.like] : `%${search}%`}},
-                    {salePaymentMethod : { [Op.like] : `%${search}%`}},
-                    {saleCity : { [Op.like] : `%${search}%`}}
+                    { saleDate: { [Op.like]: `%${search}%` } },
+                    { salePrice: { [Op.like]: `%${search}%` } },
+                    { salePaymentMethod: { [Op.like]: `%${search}%` } },
+                    { saleCity: { [Op.like]: `%${search}%` } },
+                    {'$Client.clientDocument$': { [Op.like]: `%${search}%` } },
+                    {'$Client.clientName$': { [Op.like]: `%${search}%` } },
+                    {'$Client.clientLastName$': { [Op.like]: `%${search}%` } },
+                    {'$vehiclesAs.licensePlate$': { [Op.like]: `%${search}%` } },
+                    {'$vehiclesAs.vehicleType$': { [Op.like]: `%${search}%` } },
+                    {'$vehiclesAs.brand$': { [Op.like]: `%${search}%` } },
+                    {'$vehiclesAs.model$': { [Op.like]: `%${search}%` } },
+                    {'$vehiclesAs.type$': { [Op.like]: `%${search}%` } },
+                    {'$vehiclesAs.line$': { [Op.like]: `%${search}%` } }
                 ],
-            }
+            },
         });
         res.json(sale);
     } catch (error) {
@@ -113,22 +164,26 @@ export const searchSale = async (req, res) => {
 };
 
 
-/* export const reportSale = async (req, res) => {
+export const reportSale = async (req, res) => {
     const startDateSale = new Date(req.params.startDateSale);
     const finalDateSale = new Date(req.params.finalDateSale);
     try {
         const sale = await Sale.findAll({
             where: {
-                reportDateSale: {
+                saleDate: {
                     [Op.between]: [startDateSale, finalDateSale]
                 }
             },
-            include: Vehicle
+            include: [
+                {
+                    model: Vehicle, 
+                    as: 'vehiclesAs' 
+                }
+            ],
         });
-
         res.json(sale);
     } catch (error) {
         console.error(error);
         return res.status(500).json({message : error.message});
     }
-};  */
+};
