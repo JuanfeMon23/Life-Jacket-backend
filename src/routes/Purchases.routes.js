43c0c4a117aae886f1fcf2748b167a3609e0f356
JuanDiego123m <juandiegomillancano853@gmail.com>
2023-09-15 09:57:42 -0500

    Updated routes

--- src/routes/Purchases.routes.js
+++ src/routes/Purchases.routes.js
@@ -1,11 +1,11 @@
 import {Router} from 'express';
-import { getPurchase, getPurchases, postPurchase, searchPurchases, statusPurchase } from '../controllers/Purchases.controller.js';
+import { getPurchase, getPurchases, postPurchase, reportPurchase, searchPurchases, statusPurchase } from '../controllers/Purchases.controller.js';
 import { requiredToken } from '../middlewares/validatingToken.js';
 export const PurchaseRoutes = Router();
 
-PurchaseRoutes.get('/Purchases',getPurchase );
 PurchaseRoutes.get('/Purchases',getPurchases );
 PurchaseRoutes.get('/Purchases/:idPurchase',getPurchase );
 PurchaseRoutes.post('/Purchases',postPurchase );
 PurchaseRoutes.patch('/Purchases/:idPurchase',statusPurchase );
 PurchaseRoutes.get('/Purchases/SearchE/:search',searchPurchases );
+PurchaseRoutes.get('/Sales/Report/:startDateSale/:finalDateSale', reportPurchase);
