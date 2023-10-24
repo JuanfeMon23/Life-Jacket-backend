47c73d01cdfc4ae45e01bc3560c6702dd82564b7
JuanDiego123m <juandiegomillancano853@gmail.com>
2023-10-23 19:28:57 -0500

    Update Improvements.controller.js
    The code was documented and the disabling of the improvement by itself was removed

--- src/controllers/Improvements.controller.js
+++ src/controllers/Improvements.controller.js
@@ -1,11 +1,21 @@
+/**
+ * Developer: Juan Diego Millan
+ * Email: juandiegomillancano853@gmail.com
+ * Creation Date: oct 2023
+ * 
+ * Description: This script contains functions to manage operations related to application vehicles which are: create, view, edit, disable, delete and search. Uses Express.js and Sequelize to interact with the database
+ */
+
 import { Improvements } from '../models/Improvements.model.js'
 import { Op } from 'sequelize';
 import app from '../app.js'
 import { Vehicle } from "../models/Vehicles.model.js";
 
-// Get
+// Function to get improvements the list of improvements
 export const getImprovements = async (req, res) => {
     try {
+
+        //Query the database to get the list of improvements
         const improvement = await Improvements.findAll({include : {model : Vehicle}});
         res.json(improvement);
     } catch (error) {
@@ -13,10 +23,12 @@ export const getImprovements = async (req, res) => {
     }
 };
 
-// Create
+// Function to create a new improvement in the database
 export const postImprovements = async (req, res) => {
     try {
+        // Extract data from the request body to create a improvement
         const {improvementDescription, improvementDate, improvementPrice, improvementStatus, idVehicleImprovement} = req.body
+
         const newImprovement = await Improvements.create({
             improvementDescription,
             improvementDate,
@@ -30,14 +42,17 @@ export const postImprovements = async (req, res) => {
     }
 };
 
-// Update 
+// Function to update a improvement
 export const updateImprovements = async (req, res) => {
     const {idImprovements} = req.params;
     try {
+        // Extract the improvement ID from the request parameters
         const {improvementDescription, improvementDate, improvementPrice, idVehicleImprovement} = req.body
         
+        // Find the vehicle by its ID
         const improvement = await Improvements.findByPk(idImprovements)
 
+        // If the improvement is disable it cannot be edited 
         if(improvement.improvementStatus === false) {
             return res.status(400).json({ message: 'No puedes editar esta mejora esta deshabilitada.' });
         }
@@ -54,25 +69,12 @@ export const updateImprovements = async (req, res) => {
     }
 };
 
-// Disable 
-export const statusImprovement = async (req, res) => {
-    const { idImprovements } = req.params;
-    try {
-        const improvement = await Improvements.findByPk(idImprovements)
-        improvement.improvementStatus = !improvement.improvementStatus;        
-        await improvement.save();
-        res.json(improvement);
 
-    } catch (error) {
-        return res.status(500).json({message : error.message});
-    }
-};
-
-
-//Search 
+//Function to search for vehicles based on various attributes (LicensePlate, Date) 
 export const getSearchImprovements = async (req, res) => {
     const {search} = req.params;
     try {
+        //Perform a search in the database
         const improvements = await Improvements.findAll({
             include: [
                 {
