2f81b1c1d4b3b4b9c883f832bc13f8518d88200c
JuanDiego123m <juandiegomillancano853@gmail.com>
2023-10-27 15:40:31 -0500

    Create Dashboard
    Dashboard controllers and routes were created

--- src/controllers/Dashboard.controller.js
+++ src/controllers/Dashboard.controller.js
@@ -2,63 +2,92 @@ import { sequelize } from "../database/database.js";
 import { Sale } from '../models/Sales.model.js';
 import { Purchase } from '../models/Purchases.model.js';
 import { Vehicle } from '../models/Vehicles.model.js';
-import { Client } from '../models/Clients.model.js';
+import { Improvements } from '../models/Improvements.model.js'
 import { Op } from 'sequelize';
 import app from '../app.js';
 
 
 
-export const getSalesData = async (req, res) => {
-    try {
-      const monthlySales = await Sale.findAll({
-        attributes: [
-          [sequelize.fn('MONTH', sequelize.col('saleDate')), 'month'],
-          [sequelize.fn('YEAR', sequelize.col('saleDate')), 'year'],
-          [sequelize.fn('COUNT', '*'), 'totalSales']
-        ],
-        group: ['month', 'year'],
-      });
-  
-      const formattedDataSales = monthlySales.map((result) => {
-        return {
-          month: result.getDataValue('month'),
-          year: result.getDataValue('year'),
-          totalSales: result.getDataValue('totalSales')
-        };
-      });
-  
-      res.json(formattedDataSales);
-    } catch (error) {
-      console.error('Error al obtener datos de ventas:', error);
-      res.status(500).json({ error: 'Error al obtener datos de ventas' });
-    }
-  };
+export const getSalesDataByAmount = async (req, res) => {
+  try {
+    const monthlySales = await Sale.findAll({
+      attributes: [
+        [sequelize.fn('MONTH', sequelize.col('saleDate')), 'month'],
+        [sequelize.fn('YEAR', sequelize.col('saleDate')), 'year'],
+        [sequelize.fn('SUM', sequelize.col('saleFinalPrice')), 'totalAmount']
+      ],
+      group: ['month', 'year'],
+    });
 
+    const formattedDataSales = monthlySales.map((result) => {
+      return {
+        month: result.getDataValue('month'),
+        year: result.getDataValue('year'),
+        totalAmount: result.getDataValue('totalAmount')
+      };
+    });
 
-export const getPurchasesData = async (req, res) => {
-    try {
-      const monthlyPurchases = await Purchase.findAll({
-        attributes: [
-          [sequelize.fn('MONTH', sequelize.col('purchaseDate')), 'month'],
-          [sequelize.fn('YEAR', sequelize.col('purchaseDate')), 'year'],
-          [sequelize.fn('COUNT', '*'), 'totalPurchases']
-        ],
-        group: ['month', 'year'],
-      });
-      const formattedDataPurchases = monthlyPurchases.map((result) => {
-        return {
-          month: result.getDataValue('month'),
-          year: result.getDataValue('year'),
-          totalPurchases: result.getDataValue('totalPurchases')
-        };
-      });
-  
-      res.json(formattedDataPurchases);
-    } catch (error) {
-      console.error('Error al obtener datos de compras', error);
-      res.status(500).json({ error: 'Error al obtener datos de compras' });
-    }
+    res.json(formattedDataSales);
+  } catch (error) {
+    console.error('Error al obtener datos de ventas por cantidad de dinero:', error);
+    res.status(500).json({ error: 'Error al obtener datos de ventas por cantidad de dinero' });
+  }
 };
+
+
+export const getPurchasesDataByAmount = async (req, res) => {
+  try {
+    const monthlyPurchases = await Purchase.findAll({
+      attributes: [
+        [sequelize.fn('MONTH', sequelize.col('purchaseDate')), 'month'],
+        [sequelize.fn('YEAR', sequelize.col('purchaseDate')), 'year'],
+        [sequelize.fn('SUM', sequelize.col('purchaseFinalPrice')), 'totalAmount']
+      ],
+      group: ['month', 'year'],
+    });
+
+    const formattedDataPurchases = monthlyPurchases.map((result) => {
+      return {
+        month: result.getDataValue('month'),
+        year: result.getDataValue('year'),
+        totalAmount: result.getDataValue('totalAmount')
+      };
+    });
+
+    res.json(formattedDataPurchases);
+  } catch (error) {
+    console.error('Error al obtener datos de compras por cantidad de dinero:', error);
+    res.status(500).json({ error: 'Error al obtener datos de compras por cantidad de dinero' });
+  }
+};
+
+
+export const getImprovementsDataByAmount = async (req, res) => {
+  try {
+    const monthlyImprovements = await Improvements.findAll({
+      attributes: [
+        [sequelize.fn('MONTH', sequelize.col('improvementDate')), 'month'],
+        [sequelize.fn('YEAR', sequelize.col('improvementDate')), 'year'],
+        [sequelize.fn('SUM', sequelize.col('improvementPrice')), 'totalAmount']
+      ],
+      group: ['month', 'year'],
+    });
+
+    const formattedDataImprovements = monthlyImprovements.map((result) => {
+      return {
+        month: result.getDataValue('month'),
+        year: result.getDataValue('year'),
+        totalAmount: result.getDataValue('totalAmount')
+      };
+    });
+
+    res.json(formattedDataImprovements);
+  } catch (error) {
+    console.error('Error al obtener datos de compras por cantidad de dinero:', error);
+    res.status(500).json({ error: 'Error al obtener datos de compras por cantidad de dinero' });
+  }
+};
+
 
 
 export const getTotalVehicles = async (req, res) => {
@@ -75,26 +104,6 @@ export const getTotalVehicles = async (req, res) => {
     res.status(500).json({ error: 'Error al obtener el total de vehículos' });
   }
 };
-
-
-
-export const getTotalClients = async (req, res) => {
-  try {
-    const totalClients = await Client.count({
-      where: {
-        clientStatus: true,
-      },
-    });
-
-    res.json({ totalClients });
-  } catch (error) {
-    console.error('Error al obtener el total de clientes :', error);
-    res.status(500).json({ error: 'Error al obtener el total de clientes' });
-  }
-};
-
-
-
 
 
 
--- src/routes/Dashboard.routes.js
+++ src/routes/Dashboard.routes.js
@@ -1,12 +1,20 @@
+/**
+ * Developer: Juan Diego Millan
+ * Email: juandiegomillancano853@gmail.com
+ * Creation Date: oct 2023
+ * 
+ *  Description: This script contains routes of Dashboard and its different functions
+ */
+
 import {Router} from 'express';
-import {getSalesData, getPurchasesData, getTotalVehicles, getTotalClients} from '../controllers/Dashboard.controller.js';
+import {getSalesDataByAmount, getPurchasesDataByAmount, getImprovementsDataByAmount, getTotalVehicles} from '../controllers/Dashboard.controller.js';
 
 export const DashboardRoutes = Router();
 
-DashboardRoutes.get('/Dashboard/Sales/withMonth', getSalesData);
+DashboardRoutes.get('/Dashboard/Sales/withMonth', getSalesDataByAmount);
 
-DashboardRoutes.get('/Dashboard/Purchases/withMonth', getPurchasesData);
+DashboardRoutes.get('/Dashboard/Purchases/withMonth', getPurchasesDataByAmount);
+
+DashboardRoutes.get('/Dashboard/Improvements/withMonth', getImprovementsDataByAmount);
 
 DashboardRoutes.get('/Dashboard/Vehicles/totalVehicles', getTotalVehicles);
-
-DashboardRoutes.get('/Dashboard/Clients/totalClients', getTotalClients);
