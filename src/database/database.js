a943cb15a522ef9ece736256aaa85b5de499a4dc
JuanfeMon23 <ELFuanfex@hotmail.com>
2023-11-29 15:26:48 -0500

    environment variables

--- src/database/database.js
+++ src/database/database.js
@@ -4,11 +4,11 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 export const sequelize = new Sequelize(
-    process.env.DATABASE ,
-    process.env.DB_USER || 'root' ,
-    process.env.DB_PASSWORD ||'',
+    process.env.DATABASE,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
 {
-    host : process.env.DB_HOST || 'localhost',
+    host : process.env.DB_HOST,
     dialect : 'mysql'
 })
 
